86185ded2f2d4ffd0d3009a8322b9fdf4ddd9b07
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-08-26 20:08:48 +0300

    updated schema

--- backend/models/ngo_model.py
+++ backend/models/ngo_model.py
@@ -7,7 +7,7 @@ class NGO(db.Model):
     name=db.Column(db.String(100),nullable=False)
     description=db.Column(db.String(500),nullable=True)
     email=db.Column(db.String(100),nullable=False,unique=True)
-    phone=db.Column(db.String(15),nullable=True)
+    phone=db.Column(db.String(25),nullable=True)
     address=db.Column(db.String(200),nullable=True)
     website=db.Column(db.String(100),nullable=True)
     created_at=db.Column(db.DateTime,server_default=db.func.now())
