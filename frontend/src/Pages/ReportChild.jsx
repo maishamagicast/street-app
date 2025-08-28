1b20d5fb9bf06b52fb79eedb1a8becf796329818
Klein Kiuvu Gachuhi <klein.gachuhi@student.moringaschool.com>
2025-08-28 11:01:16 +0300

    Report child form

--- frontend/src/Pages/ReportChild.jsx
+++ frontend/src/Pages/ReportChild.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function ReportChild() {
+  function handleChildReport () {
+    alert("Child has been reported")
+  }
   return (
-    <div>
+    <div className='report-child-container'>
       <form action="" className='reporting-child-form'>
-        <input type="text" placeholder='Enter child Name' className='child-name-input'/>
-        <input type="number" placeholder='Enter Age of Child' className='child-age-input' />
-        <input type="text" placeholder='Enter name of Parent'className='parent-name-input' />
-        <input type="number" placeholder="Enter parent's contact" className='parent-contact-input' />
-        <input type="text" placeholder='Last place seen' className='last-seen-input'/>
-        <button>submit</button>
+        <input type="text" placeholder='Enter child Name' className='report-child-name-input'/>
+        <input type="number" placeholder='Enter Age of Child' className='report-child-age-input' />
+        <input type="text" placeholder='Enter name of Parent'className='report-parent-name-input' />
+        <input type="number" placeholder="Enter parent's contact" className='report-parent-contact-input' />
+        <input type="text" placeholder='Last place seen' className='report-last-seen-input'/>
+        <button className='report-child-button' onClick={handleChildReport} >submit</button>
       </form>
     </div>
   )
